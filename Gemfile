f52a97bd661bd960f62d90a612b26432a7018455
Kevin Bridges <kevin@cyberswat.com>
2014-04-16 12:56:05 -0600

    Initial varnish.

---
+++ Gemfile
@@ -0,0 +1,18 @@
+# encoding: utf-8
+source 'https://rubygems.org'
+
+gem 'foodcritic', '~> 3.0'
+gem 'rake'
+gem 'rubocop'
+gem 'rspec'
+gem 'chefspec'
+gem 'berkshelf'
+gem 'net-ldap'
+gem 'travis'
+gem 'websocket-native'
+gem 'coveralls', require: false
+
+group :integration do
+  gem 'test-kitchen', '~> 1.0'
+  gem 'kitchen-vagrant'
+end
---
+++ metadata.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+name 'nmdvarnish'
+maintainer 'NewMedia! Denver'
+maintainer_email 'support@newmediadenver.com'
+license 'Apache 2.0'
+
+version '1.0.0'
+supports 'ubuntu', '= 12.04'
+
+desc = 'Manages varnish.'
+description desc
+
+desc = '@TODO'
+
+long_description desc
+
+desc = '@TODO'
+recipe 'nmdvarnish::default', desc
