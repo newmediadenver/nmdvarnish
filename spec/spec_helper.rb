b80303274d1800c9f0e855728c0477fc880101fc
David Arnold <david@newmediadenver.com>
2014-04-18 17:32:58 -0600

    updated Readme to reflect correct travis badges.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,10 @@ RSpec.configure do |config|
   # config.path = 'ohai.json'
 
   # Specify the operating platform to mock Ohai data from
-  config.platform = 'Ubuntu'
+  config.platform = 'CentOS'
 
   # Specify the operating version to mock Ohai data from
-  config.version = '12.04'
+  config.version = '5.10'
 
   # Use color output for RSpec
   config.color_enabled = true
